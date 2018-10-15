0a577e039abdf8bd8c95fd069e4528f870f58cb0
Alex Robert <alex.robert@live.fr>
2018-10-16 00:58:00 +0200

    Server use the embed module and some memory cache

--- server.js
+++ server.js
@@ -1,64 +1,34 @@
 const debug = require('debug')('ebdgram:server')
-const path = require("path");
 const express = require('express')
-
-const request = require('request-promise-native')
-const cheerio = require('cheerio')
-
-const qError = 'div.error-container'
-const qFeed = 'body script'
-
-function getFeed (profile) {
-  return request('https://www.instagram.com/' + profile)
-    .then( response => {
-      return cheerio.load(response)
-    })
-    .then( $ => {
-      debug('Loading profile : %s', profile)
-      if(!$(qFeed).length) throw new Error('cannot get profile ' + profile)
-
-      let script = $(qFeed).first().html()
-      script = script.replace('window._sharedData =', '')
-      script = script.replace(/(;)$/, '')
-
-      const data = JSON.parse(script)
-      const edges = data.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges
-
-      let thumbs = []
-
-      for(edge of edges){
-        thumbs.push(edge.node.thumbnail_src)
-      }
-
-      return thumbs
-    })
-    .catch(error => {
-      debug(error)
-    })
-}
+const mcache = require('memory-cache')
+const embed = require('./embed')
 
 const app = express()
 
 app.enable("trust proxy")
 app.get('/favicon.ico', (req, res) => res.status(204))
 
-app.use(express["static"](path.join(__dirname, "public")))
-app.set("views", path.join(__dirname, "view"))
-app.set("view engine", "pug")
-app.set("view cache", false)
 
 app.get('/:profile', (req, res, next) => {
   profile = req.params.profile
-  getFeed(profile).then(thumbs => {
-    res.render('feed', {
-      title: 'Embed : ' + profile,
-      thumbs: thumbs,
-      ig_link: 'https://www.instagram.com/' + profile
+
+  let body = mcache.get(profile)
+  if(body){
+    debug('this page is cached.')
+    res.status(200).send(body)
+    return
+  }
+
+  debug('i am not here after beeing cached, never will be unless expired.')
+
+  embed(profile)
+    .then( (body) => {
+      mcache.put(profile, body, 3600*1000)
+      res.status(200).send(body)
     })
-  })
-  .catch(error => {
-    next(error)
-  })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.use(function(err, req, res, next) {
